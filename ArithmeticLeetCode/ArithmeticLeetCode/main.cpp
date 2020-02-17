3c870a98c26c4d85e62afe960e8cd6e2972b2aa1
martint028 <tongji_you@163.com>
2020-02-17 16:38:05 +0800

    Init project

---
+++ ArithmeticLeetCode/ArithmeticLeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArithmeticLeetCode
+//
+//  Created by martin on 17/2/20.
+//  Copyright © 2020 martin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
